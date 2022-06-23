0e44506181a56cf494c92189a3351ec2a561a0d8
shay <shayosullivan@hotmail.com>
2022-06-22 21:20:49 -0400

    models finsihed

---
+++ models/Comments.js
@@ -0,0 +1,45 @@
+const { Model, DataTypes } = require("sequelize");
+const sequelize = require("../config/connection");
+
+class Comment extends Model {}
+
+Comment.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    comment_content: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    comment_date: {
+      type: DataTypes.DATE,
+      allowNull: false,
+    },
+    blog_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: "blog",
+        key: "id",
+      },
+    },
+    user_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: "user",
+        key: "id",
+      },
+    },
+  },
+  {
+    sequelize,
+    freezeTableName: true,
+    underscored: true,
+    modelName: "comment",
+  }
+);
+
+module.exports = Comment;
